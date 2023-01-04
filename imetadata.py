7e2ba701b0724f9274ca5e383ed958ad921b2f92
Kunal Walavalkar <kunalw2002@gmail.com>
2023-01-04 22:30:18 +0530

    Third commit

--- imetadata.py
+++ imetadata.py
@@ -1,7 +1,31 @@
 from PIL import Image
-from PIL.ExifTags import TAGS
+import os
 import sys
+from PIL import Image
+from PIL.ExifTags import GPSTAGS, TAGS
 import argparse
+
+
+def create_google_maps_url(gps_coords):
+    # Exif data stores coordinates in degree/minutes/seconds format. To convert to decimal degrees.
+    # We extract the data from the dictionary we sent to this function for latitudinal data.
+    dec_deg_lat = convert_decimal_degrees(float(gps_coords["lat"][0]), float(gps_coords["lat"][1]),
+                                          float(gps_coords["lat"][2]), gps_coords["lat_ref"])
+    # We extract the data from the dictionary we sent to this function for longitudinal data.
+    dec_deg_lon = convert_decimal_degrees(float(gps_coords["lon"][0]), float(gps_coords["lon"][1]),
+                                          float(gps_coords["lon"][2]), gps_coords["lon_ref"])
+    # We return a search string which can be used in Google Maps
+    return f"https://maps.google.com/?q={dec_deg_lat},{dec_deg_lon}"
+
+
+# Converting to decimal degrees for latitude and longitude is from degree/minutes/seconds format is the same for latitude and longitude. So we use DRY principles, and create a seperate function.
+def convert_decimal_degrees(degree, minutes, seconds, direction):
+    decimal_degrees = degree + minutes / 60 + seconds / 3600
+    # A value of "S" for South or West will be multiplied by -1
+    if direction == "S" or direction == "W":
+        decimal_degrees *= -1
+    return decimal_degrees
+
 
 parser = argparse.ArgumentParser(description="Simple Image Metadata Viewer")
 
@@ -10,37 +34,45 @@ parser.add_argument("image", help="Path of image relative to script. Path should
 args = parser.parse_args()
 
 # Path to the image or video
-imagename = args.image
+file = args.image
 
-# Reading the image data using PIL
-image = Image.open(imagename)
+try:
+    # Open the image file. We open the file in binary format for reading.
+    image = Image.open(file)
+    # The ._getexif() method returns a dictionary. .items() method returns a list of all dictionary keys and values.
+    gps_coords = {}
+    # We check if exif data are defined for the image.
+    if image._getexif() == None:
+        print(f"{file} contains no exif data.")
+    # If exif data are defined we can cycle through the tag, and value for the file.
+    else:
+        for tag, value in image._getexif().items():
+            # If you print the tag without running it through the TAGS.get() method you'll get numerical values for every tag. We want the tags in human-readable form.
+            # You can see the tags and the associated decimal number in the exif standard here: https://exiv2.org/tags.html
+            tag_name = TAGS.get(tag)
+            if tag_name == "GPSInfo":
+                for key, val in value.items():
+                    # Print the GPS Data value for every key to the screen.
+                    print(f"{GPSTAGS.get(key)} - {val}")
+                    # We add Latitude data to the gps_coord dictionary which we initialized in line 110.
+                    if GPSTAGS.get(key) == "GPSLatitude":
+                        gps_coords["lat"] = val
+                    # We add Longitude data to the gps_coord dictionary which we initialized in line 110.
+                    elif GPSTAGS.get(key) == "GPSLongitude":
+                        gps_coords["lon"] = val
+                    # We add Latitude reference data to the gps_coord dictionary which we initialized in line 110.
+                    elif GPSTAGS.get(key) == "GPSLatitudeRef":
+                        gps_coords["lat_ref"] = val
+                    # We add Longitude reference data to the gps_coord dictionary which we initialized in line 110.
+                    elif GPSTAGS.get(key) == "GPSLongitudeRef":
+                        gps_coords["lon_ref"] = val
+            else:
+                # We print data not related to the GPSInfo.
+                print(f"{tag_name} - {value}")
+        # We print the longitudinal and latitudinal data which has been formatted for Google Maps. We only do so if the GPS Coordinates exists.
+        if gps_coords:
+            print(create_google_maps_url(gps_coords))
+        # Change back to the original working directory.
+except IOError:\
+    print("File format not supported!")
 
-# Extracting the basic metadata
-info_dict = {
-    "Filename": image.filename,
-    "Image Size": image.size,
-    "Image Height": image.height,
-    "Image Width": image.width,
-    "Image Format": image.format,
-    "Image Mode": image.mode,
-    "Image is Animated": getattr(image, "is_animated", False),
-    "Frames in Image": getattr(image, "n_frames", 1)
-}
-
-for label, value in info_dict.items():
-    print(f"{label:25}: {value}")
-
-# extracting EXIF data
-exifdata = image.getexif()
-
-# Iterating over all EXIF data fields
-for tag_id in exifdata:
-
-    # Getting the tag name, instead of human unreadable tag id
-    tag = TAGS.get(tag_id, tag_id)
-    data = exifdata.get(tag_id)
-
-    # Decoding bytes
-    if isinstance(data, bytes):
-        data = data.decode()
-    print(f"{tag:25}: {data}")
